5f5df202566b4dcc7c341e33a0b5a3f92dc40b84
Timur Gilyazov <angrywips96@gmail.com>
2024-03-03 14:16:11 +0300

    1.00

--- Holodilnik/Holodilnik.py
+++ Holodilnik/Holodilnik.py
@@ -1,17 +1,22 @@
 import datetime
 import pickle
 
-
 DATE_FORMAT = "%d.%m.%Y"
 with open("dict.txt", "rb+") as f:
     goods = pickle.load(f)
 
 
-def add(items):
+def add(items: dict) -> None:
+    """Функция добовляет продукт в холодильник"""
+
     title = input("""Введите название продукта: """).strip()
     amount = input("Введите кол-во добовляемого продукта: ")
     date = datetime.datetime.strptime(
-        input("Введите срок годности продукта в формате DD.MM.YYYY: "), DATE_FORMAT
+        input(
+            "Введите срок годности продукта в формате\
+            DD.MM.YYYY: "
+        ),
+        DATE_FORMAT,
     ).date()
     if title not in items:
         items[title] = [{"Кол-во:": amount, "Годен до:": date}]
@@ -23,14 +28,13 @@ def choice():
     print(
         """
     ------------------------------------------
-    Выберите нужную функцию :   
-      1. Добавить продукт в холодильник 
-      2. Информация о продуктах в холодильнике 
-      3. Забрать продукт из холодильника 
-      4. Найти продукт в холодильнике 
-    ------------------------------------------  
-    
-    """
+    Выберите нужную функцию :
+      1. Добавить продукт в холодильник
+      2. Информация о продуктах в холодильнике
+      3. Забрать продукт из холодильника
+      4. Найти продукт в холодильнике
+    ------------------------------------------
+   """
     )
     input_choise = input("Введите номер операции: ")
     if input_choise == "1":
@@ -44,14 +48,15 @@ def expire(items, in_advance_days=7):
     print()
 
     for item in items:
-        for object in items[item]:
-            if object["Годен до:"] != "-":
-                delta = object["Годен до:"] - datetime.datetime.now().date()
+        for food in items[item]:
+            if food["Годен до:"] != "-":
+                delta = food["Годен до:"] - datetime.datetime.now().date()
                 if delta.days <= in_advance_days and delta.days > 0:
-                    amount = object["Кол-во:"]
-                    object_expire = object["Годен до:"].strftime(DATE_FORMAT)
+                    amount = food["Кол-во:"]
+                    food_expire = food["Годен до:"].strftime(DATE_FORMAT)
                     print(
-                        f"    {count}. {item} в кол-ве {amount}, Дата окончания срока годности: {object_expire} "
+                        f"    {count}. {item} в кол-ве {amount}, Дата \
+                            окончания срока годности: {food_expire}"
                     )
                     count += 1
     count = 1
@@ -60,26 +65,27 @@ def expire(items, in_advance_days=7):
     print("    Пора выбрасывать:")
     print()
     for item in items:
-        for object in items[item]:
-            if object["Годен до:"] != "-":
-                delta = object["Годен до:"] - datetime.datetime.now().date()
+        for food in items[item]:
+            if food["Годен до:"] != "-":
+                delta = food["Годен до:"] - datetime.datetime.now().date()
                 if delta.days < 0:
-                    amount = object["Кол-во:"]
-                    object_expire = object["Годен до:"].strftime(DATE_FORMAT)
+                    amount = food["Кол-во:"]
+                    food_expire = food["Годен до:"].strftime(DATE_FORMAT)
                     print(
-                        f"    {count}. {item} в кол-ве {amount}, Дата окончания срока годности: {object_expire} "
+                        f"{count}. {item} в кол-ве {amount}, Дата окончания \
+                            срока годности: {food_expire} "
                     )
                     count += 1
 
 
 print('Здравствуйте! Вас приветствует приложение "Холодильник v 2.0"!')
 print(
-    f""" 
-      
+    f"""
+
     ---------------------------------------
-      
+
     На данный момент в холодильнике находится {len(goods)} видов продуктов.
-      
+
       """
 )
 
